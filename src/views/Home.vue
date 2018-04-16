f16c4e75768a979c60a877a63dfb3a2fc59ab5a0
Andrey Suvorkin <andrejwest@mail.ru>
2018-04-16 21:51:24 +0300

    Frontend: test commit to development branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to my Vue.js App"/>
   </div>
 </template>
 
