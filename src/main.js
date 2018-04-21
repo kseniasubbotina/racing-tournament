97018ae967f02db28311e5cd209bd55ade65b464
Andrey Suvorkin <andrejwest@mail.ru>
2018-04-21 21:04:30 +0300

    Frontend: vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/src/stylus/main.styl'
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Championships.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-flex>
+  	Championship page!
+  </v-flex>
+</template>
