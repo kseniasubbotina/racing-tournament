ef08c11dcb06e136ca37d436d4b721a78e1fe1ca
Maksim Ivanov <mak.ivanov@gmail.com>
2018-05-12 18:58:58 +0300

    added fields on the championship form, plus added a picture-input module

--- src/views/Championships.vue
+++ src/views/Championships.vue
@@ -7,8 +7,47 @@
       <v-container grid-list-sm class="pa-4">
         <v-form>
           <v-layout>
-            <v-flex xs12 sm6 justify-space-between>
-              <v-text-field ></v-text-field>
+            <v-flex xs12 sm6>
+              <v-text-field
+                label="Title"
+                v-model="title"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-select
+                :items="gameTypesArray"
+                v-model="gameType"
+                label="Game types"
+                single-line
+              ></v-select>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12>
+              <v-text-field
+                v-model="gameDescription"
+                label="Game description"
+                textarea
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12>
+              <p>Championship image</p>
+              <picture-input
+                ref="pictureInput"
+                @change="onChanged"
+                width="200"
+                height="200"
+                margin="16"
+                accept="image/jpeg,image/png"
+                size="10"
+                :removable="true"
+                :customStrings="{
+                  upload: '<h1>This image has been loaded</h1>',
+                  drag: 'Drag an image here'
+                }">
+              </picture-input>
             </v-flex>
           </v-layout>
         </v-form>
@@ -18,7 +57,36 @@
 </template>
 
 <script>
+import PictureInput from 'vue-picture-input'
+
 export default {
-  name: 'Championships'
+  name: 'Championships',
+  data: () => ({
+    title: null,
+    gameTypesArray: ['Game1', 'Game2', 'Game3'],
+    gameType: null,
+    gameDescription: null
+  }),
+  methods: {
+    onChanged: function () {
+      console.log('New picture loaded')
+      if (this.$refs.pictureInput.file) {
+        this.image = this.$refs.pictureInput.file
+      } else {
+        console.log('Old browser. No support for Filereader API')
+      }
+    },
+    onRemoved: function () {
+      this.image = ''
+    },
+    attemptUpload: function () {
+      if (this.image) {
+        // axios
+      }
+    }
+  },
+  components: {
+    PictureInput
+  }
 }
 </script>
