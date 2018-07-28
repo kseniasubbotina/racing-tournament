b01de2a1fc13ef21b0eb905813637261fb9a5a4b
Max Ivanov <mak.ivanov@gmail.com>
2018-07-28 11:47:48 +0300

    added a new remote connection to db

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const {sequelize} = require('./models')
-const config = require('./config/config')
 
+// set up express app
 const app = express()
-app.use(morgan('combined'))
+
+// initialize routes
+app.use('/', require('./routes'))
+
+// use body-parser middleware
 app.use(bodyParser.json())
-app.use(cors())
 
-require('./routes')(app)
+// listen for requests
+app.listen(process.env.port || 4000, function () {
+  console.log('now listening for requests')
+})
 
-sequelize.sync()
-  .then(() => {
-    app.listen(config.port || 8080)
-    console.log(`Server started on ${config.port}`)
-  })
+// FREE CODE CAMP EXAMPLE
+//
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const morgan = require('morgan')
+// const {sequelize} = require('./models')
+// const config = require('./config/config')
+
+// const app = express()
+// app.use(morgan('combined'))
+// app.use(bodyParser.json())
+// app.use(cors())
+
+// require('./routes')(app)
+
+// sequelize.sync()
+//   .then(() => {
+//     app.listen(config.port || 8080)
+//     console.log(`Server started on ${config.port}`)
+//   })
