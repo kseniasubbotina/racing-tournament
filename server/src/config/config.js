9e81c11a8c128a5281a185f2395decc16b0d6494
Max Ivanov <mak.ivanov@gmail.com>
2018-07-28 11:47:48 +0300

    added a new remote connection to db

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,13 +1,24 @@
-module.exports = {
-  port: process.env.PORT || 8080,
-  db: {
-    database: process.env.DB_NAME || 'tournament',
-    user: process.env.DB_USER || 'tournament',
-    password: process.env.DB_PASS || 'tournament',
-    options: {
-      dialect: process.env.DIALECT || 'sqlite',
-      host: process.env.HOST || 'localhost',
-      torage: './tournament.sqlite'
-    }
-  }
-}
+// module.exports = {
+//   port: process.env.PORT || 8080,
+//   db: {
+//     database: process.env.DB_NAME || 'tournament',
+//     user: process.env.DB_USER || 'tournament',
+//     password: process.env.DB_PASS || 'tournament',
+//     options: {
+//       dialect: process.env.DIALECT || 'sqlite',
+//       host: process.env.HOST || 'localhost',
+//       torage: './tournament.sqlite'
+//     }
+//   }
+// }
+
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+  host: 'andrej0h.beget.tech',
+  user: 'andrej0h_rt',
+  password: 've*JWq3c',
+  database: 'andrej0h_rt'
+})
+
+module.exports = con
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,25 @@
-const AuthentificationController = require('./controllers/AuthentificationController')
+// const AuthentificationController = require('./controllers/AuthentificationController')
 
-module.exports = (app) => {
-  app.post('/register',
-    AuthentificationController.register)
-  app.get('/login', function (req, res) {
-    res.send('Hi')
+// module.exports = (app) => {
+//   app.post('/register',
+//     AuthentificationController.register)
+//   app.get('/login', function (req, res) {
+//     res.send('Hi')
+//   })
+// }
+
+const express = require('express')
+const router = express.Router()
+const con = require('./config/config')
+
+router.get('/races', function (req, res, next) {
+  con.connect(function (err) {
+    if (err) throw err
+    con.query('SELECT * FROM races', function (err, result, fields) {
+      if (err) throw err
+      res.send(result)
+    })
   })
-}
+})
+
+module.exports = router
