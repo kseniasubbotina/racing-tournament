13ee01eac2ba78d990656c793adfe74ffd80b925
Andrey Suvorkin <andrejwest@mail.ru>
2018-07-29 23:31:02 +0300

    config.js code improvement

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,17 +1,3 @@
-// module.exports = {
-//   port: process.env.PORT || 8080,
-//   db: {
-//     database: process.env.DB_NAME || 'tournament',
-//     user: process.env.DB_USER || 'tournament',
-//     password: process.env.DB_PASS || 'tournament',
-//     options: {
-//       dialect: process.env.DIALECT || 'sqlite',
-//       host: process.env.HOST || 'localhost',
-//       torage: './tournament.sqlite'
-//     }
-//   }
-// }
-
 var mysql = require('mysql')
 module.exports = {
   connection: mysql.createConnection({
