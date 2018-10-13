14fa9eb684d1794787b52e5a1915e12df9b087a4
Andrey Suvorkin <andrejwest@mail.ru>
2018-10-13 13:47:05 +0300

    Update client/src/store.js

--- client/src/store.js
+++ client/src/store.js
@@ -12,17 +12,17 @@ export default new Vuex.Store({
     loading: false
   },
   mutations: {
-    set (state, {type, val}) {
+    set (state, { type, val }) {
       state[type] = val
     }
   },
   actions: {
-    signUp ({commit}, credentials) {
-      commit('set', {type: 'loading', val: true})
+    signUp ({ commit }, credentials) {
+      commit('set', { type: 'loading', val: true })
       fb.auth.createUserWithEmailAndPassword(credentials.email, credentials.password).then(
         response => {
           if (response) {
-            commit('set', {type: 'error', val: null})
+            commit('set', { type: 'error', val: null })
             fb.usersCollection.doc(response.user.uid).set({
               username: credentials.username,
               country: credentials.country,
@@ -34,82 +34,82 @@ export default new Vuex.Store({
               'id': response.user.uid,
               'email': response.user.email
             }
-            commit('set', {type: 'user', val: newUser})
-            commit('set', {type: 'loading', val: false})
-            commit('set', {type: 'message', val: null})
+            commit('set', { type: 'user', val: newUser })
+            commit('set', { type: 'loading', val: false })
+            commit('set', { type: 'message', val: null })
             this.dispatch('fetchUserData')
           }
         },
         function (error) {
-          commit('set', {type: 'loading', val: false})
-          commit('set', {type: 'message', val: error.message})
+          commit('set', { type: 'loading', val: false })
+          commit('set', { type: 'message', val: error.message })
         })
     },
-    signIn ({commit}, credentials) {
-      commit('set', {type: 'loading', val: true})
+    signIn ({ commit }, credentials) {
+      commit('set', { type: 'loading', val: true })
       fb.auth.signInWithEmailAndPassword(credentials.email, credentials.password).then(
         response => {
           const newUser = {
             'id': response.user.uid,
             'email': response.user.email
           }
-          commit('set', {type: 'user', val: newUser})
-          commit('set', {type: 'loading', val: false})
-          commit('set', {type: 'message', val: null})
+          commit('set', { type: 'user', val: newUser })
+          commit('set', { type: 'loading', val: false })
+          commit('set', { type: 'message', val: null })
           this.dispatch('fetchUserData')
         },
         function (error) {
-          commit('set', {type: 'message', val: error.message})
-          commit('set', {type: 'loading', val: false})
+          commit('set', { type: 'message', val: error.message })
+          commit('set', { type: 'loading', val: false })
         })
     },
-    autoSignIn ({commit}, credentials) {
-      commit('set', {type: 'loading', val: true})
+    autoSignIn ({ commit }, credentials) {
+      commit('set', { type: 'loading', val: true })
       const newUser = {
         'id': credentials.uid,
         'email': credentials.email
       }
-      commit('set', {type: 'user', val: newUser})
-      commit('set', {type: 'loading', val: false})
+      commit('set', { type: 'user', val: newUser })
+      commit('set', { type: 'loading', val: false })
     },
-    signOut ({commit}) {
+    signOut ({ commit }) {
       fb.auth.signOut().then(function () {
-        commit('set', {type: 'user', val: null})
+        commit('set', { type: 'user', val: null })
       }).catch(function (error) {
-        commit('set', {type: 'message', val: error})
+        commit('set', { type: 'message', val: error })
       })
     },
-    fetchUserData ({commit}) {
+    fetchUserData ({ commit }) {
       fb.usersCollection.doc(this.state.user.id).get().then(res => {
-        commit('set', {type: 'userData', val: res.data()})
+        commit('set', { type: 'userData', val: res.data() })
       }).catch(err => {
         console.log(err)
-        commit('set', {type: 'message', val: err.message})
+        commit('set', { type: 'message', val: err.message })
       })
     },
-    updateProfile ({commit}, newDetails) {
-      commit('set', {type: 'loading', val: true})
+    updateProfile ({ commit }, newDetails) {
+      commit('set', { type: 'loading', val: true })
       fb.usersCollection.doc(this.state.user.id).update({
         country: newDetails.country,
         username: newDetails.username,
         avatarURL: newDetails.avatarURL
       }).then(function () {
         console.log('Document successfully updated!')
-        commit('set', {type: 'message', val: 'Information successfully updated!'})
-        commit('set', {type: 'loading', val: false})
+        commit('set', { type: 'message', val: 'Information successfully updated!' })
+        commit('set', { type: 'loading', val: false })
       }).catch(function (error) {
-        commit('set', {type: 'message', val: error})
-        commit('set', {type: 'loading', val: false})
+        commit('set', { type: 'message', val: error })
+        commit('set', { type: 'loading', val: false })
       })
       fb.usersCollection.doc(this.state.user.id).onSnapshot(doc => {
         this.dispatch('fetchUserData')
       })
     },
-    clearData ({commit}) {
-      commit('set', {type: 'message', val: null})
-      commit('set', {type: 'message', val: null})
-      commit('set', {type: 'user', val: null})
-      commit('set', {type: 'loading', val: false})
+    clearData ({ commit }) {
+      commit('set', { type: 'message', val: null })
+      commit('set', { type: 'message', val: null })
+      commit('set', { type: 'user', val: null })
+      commit('set', { type: 'loading', val: false })
     }
   },
   getters: {
