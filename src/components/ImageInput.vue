df9aa077808921e1c93bdd57bd0796ae35abde75
Andrey Suvorkin <andrejwest@mail.ru>
2018-12-25 23:30:27 +0300

    Update ImageInput.vue

--- src/components/ImageInput.vue
+++ src/components/ImageInput.vue
@@ -1,20 +1,24 @@
 <template>
-  <v-flex xs12 class="text-xs-center">
-    <div v-if="selectedFile">{{selectedFile.name}}</div>
-    <div v-else></div>
-    <v-layout justify-center align-center column wrap>
-      <v-flex>
-        <img width="200px" :src="previewUrl" alt>
-        <br>
-      </v-flex>
-      <v-flex>
-        <v-btn @click="$refs.filenput.click()" flat>Browse</v-btn>
-        <v-btn v-if="url || _url" flat color="error" @click="deleteImage">Delete</v-btn>
-        <input style="display: none" ref="filenput" type="file" @change="onFileSelected">
-      </v-flex>
-      <!-- <message/> -->
-    </v-layout>
-  </v-flex>
+  <v-layout class="image-input">
+    <v-flex xs12 class="text-xs-center">
+      <div v-if="selectedFile">{{selectedFile.name}}</div>
+      <div v-else></div>
+      <v-layout justify-center align-center column wrap>
+        <v-flex style="cursor: pointer" @click="$refs.filenput.click()">
+          <img v-if="previewUrl" width="200px" :src="previewUrl" alt>
+          <div v-else alt>
+            <v-icon style="font-size: 40px">collections</v-icon>
+          </div>
+        </v-flex>
+        <v-flex>
+          <v-btn @click="$refs.filenput.click()" flat>Browse</v-btn>
+          <v-btn v-if="url || _url" flat color="error" @click="deleteImage">Delete</v-btn>
+          <input style="display: none" ref="filenput" type="file" @change="onFileSelected">
+        </v-flex>
+        <!-- <message/> -->
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -44,7 +48,6 @@ export default {
       let type = event.target.files[0].type
       if (type == 'image/png' || type == 'image/jpg' || type == 'image/jpeg') {
         this.selectedFile = event.target.files[0]
-        debugger
         this.url = URL.createObjectURL(this.selectedFile)
         this.$emit('fileSelected', this.selectedFile)
       } else {
