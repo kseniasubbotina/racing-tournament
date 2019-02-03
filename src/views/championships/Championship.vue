70d851724713b9533e7d468e3ed8b8801208f941
Andrey Suvorkin <andrejwest@mail.ru>
2019-02-03 20:46:06 +0300

    Update Championship.vue

--- src/views/championships/Championship.vue
+++ src/views/championships/Championship.vue
@@ -3,10 +3,27 @@
     <div v-if="loading" class="text-xs-center">
       <v-progress-circular :size="50" color="red" indeterminate></v-progress-circular>
     </div>
-    <v-container v-if="championship">
-      {{championship.info.name}}
-      Drivers: {{championship.drivers.length}}/{{championship.info.playersCount}}
-    </v-container>
+
+    <v-card>
+      <v-container v-if="championship">
+        <v-layout>
+          <v-flex>
+            <h1>{{championship.info.name}}</h1>
+            <v-spacer></v-spacer>
+          </v-flex>
+          <v-flex>Approved: {{championship.approved}}</v-flex>
+        </v-layout>
+        <v-layout column>
+          <h3>{{championship.info.game}}</h3>
+          {{championship.info.description}}
+          Drivers: {{championship.drivers.length}}/{{championship.info.playersCount}}
+        </v-layout>
+        <div
+          v-for="stage in championship.calendar"
+          :key="stage.documentId"
+        >{{stage.track}} - {{stage.date}} {{stage.time}}</div>
+      </v-container>
+    </v-card>
   </div>
 </template>
 
