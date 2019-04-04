6d18acd59e3044b7677cc155af97c3ad3c878e66
Andrey Suvorkin <andrejwest@mail.ru>
2019-04-04 22:42:38 +0300

    Update Message.vue

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,26 +1,7 @@
 <template>
   <v-layout>
-    {{dialog}}
-    <v-dialog v-model="dialog" max-width="290">
-      <v-card>
-        <!-- <v-card-title class="headline">Use Google's location service?</v-card-title> -->
-        <v-card-text>
-          <span class="green--text">{{message.success}}</span>
-          <span class="red--text">{{message.error}}</span>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn color="green darken-1" flat="flat" @click="dialog = false">Disagree</v-btn>
-
-          <v-btn color="green darken-1" flat="flat" @click="dialog = false">Agree</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
     <v-flex>
-      <!-- <span class="green--text">{{message.success}}</span>
-      <span class="red--text">{{message.error}}</span>-->
+      <v-alert v-if="message" :value="true" :type="message.type">{{message.text}}</v-alert>
     </v-flex>
   </v-layout>
 </template>
@@ -33,36 +14,24 @@ export default {
       dialog: false
     }
   },
-  // watch: {
-  //   message (val) {
-  //     debugger
-  //     if (val.success || val.error) {
-  //       this.dialog = true
-  //     } else {
-  //       this.dialog = false
-  //     }
-  //   }
-  // },
   computed: {
     message () {
-      this.dialogShow(this.$store.getters.message)
-      return this.$store.getters.message
+      if(this.$store.getters.message.error) {
+        return {type: 'error', text: this.$store.getters.message.error}
+      } else if (this.$store.getters.message.success) {
+        return {type: 'success', text: this.$store.getters.message.success}
+      } else {
+        return null
+      }
     }
   },
   beforeDestroy () {
     this.clearMessage()
   },
   methods: {
-    dialogShow (val) {
-      debugger
-      if (val.success || val.error) {
-        this.dialog = true
-      } else {
-        this.dialog = false
-      }
-    },
     clearMessage () {
-      this.$store.commit('set', {type: 'message', val: {error: '', success: ''}})
+      this.$store.commit('setMessage', { type: 'error', text: '' })
+      this.$store.commit('setMessage', { type: 'success', text: '' })
     }
   }
 }
