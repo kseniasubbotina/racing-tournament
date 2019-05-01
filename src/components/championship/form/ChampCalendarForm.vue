4f104334ec8593d567fa4f0ed94fbad63cd12c52
Andrey Suvorkin <andrejwest@mail.ru>
2019-05-01 23:08:26 +0300

    Update ChampCalendarForm.vue

--- src/components/championship/form/ChampCalendarForm.vue
+++ src/components/championship/form/ChampCalendarForm.vue
@@ -22,7 +22,6 @@
 </template>
 
 <script>
-import convertDateTime from '@/mixins/convertDateTime.js'
 import StageItemForm from '@/components/form-elements/StageItemForm.vue'
 export default {
   name: 'ChampCalendarForm',
@@ -106,31 +105,7 @@ export default {
         this.stages.splice(index, 1)
       }
     }
-    // dateTimeToUtc (date, time, type) {
-    //   if(date && time) {
-    //     let dateArr = date.split('-')
-    //     let timeArr = time.split(':')
-    //     let localStageDateTIme = new Date(dateArr[0], dateArr[1]-1, dateArr[2], timeArr[0], timeArr[1])
-    //     let year = localStageDateTIme.getUTCFullYear()
-    //     let month = localStageDateTIme.getUTCMonth()
-    //     let day = localStageDateTIme.getUTCDate()
-    //     let hours = localStageDateTIme.getUTCHours()
-    //     let minutes = localStageDateTIme.getUTCMinutes()
-    //     // let utcDateTime = year + ', ' + month + ', ' + day + ', ' + hours + ', ' + minutes
-    //     if(type == 'date') {
-    //       return year + '-' + month + '-' + day
-    //     } else if(type == 'time') {
-    //       return hours + ':' + minutes
-    //     }
-    //     // return utcDateTime.toString()
-    //   } else {
-    //     return ''
-    //   }
-    // }
   },
-  mixins: [
-    convertDateTime
-  ],
   components: {
     StageItemForm
   }
