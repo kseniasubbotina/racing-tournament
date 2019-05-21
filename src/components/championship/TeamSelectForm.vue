d959d0827c959c5aeff1ff292d3648eb2515986e
Andrey Suvorkin <andrejwest@mail.ru>
2019-05-21 23:51:27 +0300

    Join form

--- src/components/championship/TeamSelectForm.vue
+++ src/components/championship/TeamSelectForm.vue
@@ -1,16 +1,39 @@
 <template>
   <v-card>
     <v-card-title class="py-4 title">{{username}}, please select a team</v-card-title>
+    <v-card-text>
+      <!-- {{selectedTeamDrivers}} -->
+      <!-- <div class="title mb-3">Drivers in</div> -->
+      <v-layout align-center justify-center>
+        <v-flex class="pl-3">
+          <v-layout column class="mb-3">
+            <div class="caption">Drivers: {{selectedTeamDrivers.length}}/{{team.places}}</div>
+            <v-flex v-for="(teamDriver, index) in selectedTeamDrivers" :key="teamDriver.id">
+              <div class="subheading">{{index+1}} - {{teamDriver.username}}</div>
+            </v-flex>
+            <div v-if="!selectedTeamDrivers.length">No drivers yet</div>
+          </v-layout>
+        </v-flex>
+        <v-flex>
+          <img v-if="team" :src="team.teamLogo" width="150" alt>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+    <v-alert v-if="isteamFulfilled" type="error" :value="true">
+      <div>There are no places in this team</div>
+    </v-alert>
     <v-container>
       <v-form>
         <TeamSelect :_seria="_seria" @changeTeam="onChangeTeam"/>
       </v-form>
     </v-container>
-    <v-alert type="info" :value="true">
+    <!-- <v-alert type="info" :value="true">
       <div>You will not be able to change a team after joining the championship</div>
-    </v-alert>
+    </v-alert>-->
     <v-card-actions>
-      <v-btn :disabled="isDisabled" @click="join">Confirm</v-btn>
+      <v-btn flat color="error" @click>Cancel</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn :disabled="isteamFulfilled" @click="join">Confirm</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -22,7 +45,7 @@ export default {
   name: 'TeamSelectForm',
   data() {
     return {
-      team: null
+      team: {}
     }
   },
   props: {
@@ -31,6 +54,23 @@ export default {
     _drivers: Object
   },
   computed: {
+    isteamFulfilled () {
+      if(this.team) {
+        debugger
+        if(this.selectedTeamDrivers.length < this.team.places) {
+          return false
+        } else {
+          return true
+        }
+      }
+    },
+    selectedTeamDrivers () {
+      if(this.team) {
+        let drivers = Object.values(this._drivers)
+        let selectedTeamDrivers = drivers.filter(driver => driver.team.name === this.team.name)
+        return selectedTeamDrivers
+      }
+    },
     username() {
       return this.$store.getters.userData.username
     },
