67203dc2273579272f1979514dab0eb70ff758b8
Andrey Suvorkin <andrey.suvorkin@altenar.com>
2019-06-06 00:49:07 +0300

    Update Intro.vue

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="primary" dark>
+  <v-card>
     <v-container>
       <v-layout justify-center align-center column>
         <h1>Welcome to F1-esport.com</h1>
@@ -7,8 +7,8 @@
           class="text-xs-center subheading mb-3"
         >Create your account to take a part in championships or create your own!</v-flex>
         <v-flex>
-          <v-btn color="green">Login</v-btn>or
-          <v-btn color="red">Register</v-btn>
+          <v-btn dark color="green">Login</v-btn>or
+          <v-btn dark color="red">Register</v-btn>
         </v-flex>
         <v-flex>
           <v-btn flat>About the project</v-btn>
