52d02634cafd8f061185733e2fb4172f61e155db
Andrey Suvorkin <andrey.suvorkin@altenar.com>
2019-06-09 23:24:41 +0300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,13 @@
     <v-card-title>
       <H1>Welcome to F1-esport.com</H1>
     </v-card-title>
-    <v-card-text>This project</v-card-text>
+    <v-card-text>
+      <div class="subtitle">
+        I am pleased to welcome you on f1-esport.com web-site.
+        <br>I created this site to unite drivers from all of the world and collect their experience and achievements in one place. Here you can create your own racing championships or take part in created ones, competing for the title of the best driver among all participants, not only in your championship. Your driver rating depends on success in the championships and is summarized in your profile so that you can then examine your own statistics and compete for higher positions in the site leaderboard. Also I hope the project will be very useful to the organizers of the leagues, providing convenient tools for administering championships. In the future, we plan to introduce new features and actively develop the project.
+        <br>Good luck in the race!
+      </div>
+    </v-card-text>
   </v-card>
 </template>
 
