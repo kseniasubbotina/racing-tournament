d4c0cf59502f5b3165f4dbbf324154e093ad89fa
Andrey Suvorkin <andrey.suvorkin@altenar.com>
2019-06-09 23:07:54 +0300

    About page

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -7,11 +7,11 @@
           class="text-xs-center subheading mb-3"
         >Take a part in championships or create your own!</v-flex>
         <v-flex>
-          <v-btn @click="$router.push('/login')" dark color="green">Login</v-btn>or
-          <v-btn @click="$router.push('/register')" dark color="red">Register</v-btn>
+          <v-btn to="/login" dark color="green">Login</v-btn>or
+          <v-btn to="/register" dark color="red">Register</v-btn>
         </v-flex>
         <v-flex>
-          <v-btn @click="$router.push('/about')" flat>About the project</v-btn>
+          <v-btn to="/about" flat>About the project</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>About page</div>
+  <v-card>
+    <v-card-title>
+      <H1>Welcome to F1-esport.com</H1>
+    </v-card-title>
+    <v-card-text>This project</v-card-text>
+  </v-card>
 </template>
 
 <script>
